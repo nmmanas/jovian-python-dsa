b4b7a4cb5952e798d32a1e3363ab686856d45d8a
Manas <nmmanas@gmail.com>
2024-07-31 17:44:05 +0530

    l4: knapsack: recursive solution

--- lesson_4_knapsack_problem_0_1.py
+++ lesson_4_knapsack_problem_0_1.py
@@ -37,14 +37,28 @@ class Solution:
     Output:
     `max_rating`: maximum possible rating that can be obtained for the given budget
     """
-    def maximum_rating_recursive(self, costs: List[int], ratings: List[int], budget: int) -> int:
+    def maximum_rating_recursive(self, costs: List[int], ratings: List[int], budget: int, idx: int = 0) -> int:
         """
         Pseudo Code (Step 3):
+        1. Process one by one ie. idx=0...n-1
+        2. if costs[idx] > remaining budget, don't include it
+        3. else we have two options,
+            a. include current one
+            b. don't include current
+        4. get the max of both options
+        5. base case is idx==n
 
         Analyze Complexity (Step 5):
         
         """
-        pass
+        if idx==len(costs):
+            return 0
+        elif costs[idx] > budget:
+            return self.maximum_rating_recursive(costs, ratings, budget, idx+1)
+        else:
+            option1 = ratings[idx] + self.maximum_rating_recursive(costs, ratings, budget-costs[idx], idx+1)
+            option2 = self.maximum_rating_recursive(costs, ratings, budget, idx+1)
+        return max(option1, option2)
 
 def load_test_cases():
     """
@@ -123,54 +137,54 @@ def load_test_cases():
     # 6. test cases from jovian
     test0 = {
         'input': {
-            'costs': 165,
-            'ratings': [23, 31, 29, 44, 53, 38, 63, 85, 89, 82],
-            'budget': [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
+            'budget': 165,
+            'costs': [23, 31, 29, 44, 53, 38, 63, 85, 89, 82],
+            'ratings': [92, 57, 49, 68, 60, 43, 67, 84, 87, 72]
         },
         'output': 309
     }
 
     test1 = {
         'input': {
-            'costs': 3,
-            'ratings': [4, 5, 6],
-            'budget': [1, 2, 3]
+            'budget': 3,
+            'costs': [4, 5, 6],
+            'ratings': [1, 2, 3]
         },
         'output': 0
     }
 
     test2 = {
         'input': {
-            'costs': 4,
-            'ratings': [4, 5, 1],
-            'budget': [1, 2, 3]
+            'budget': 4,
+            'costs': [4, 5, 1],
+            'ratings': [1, 2, 3]
         },
         'output': 3
     }
 
     test3 = {
         'input': {
-            'costs': 170,
-            'ratings': [41, 50, 49, 59, 55, 57, 60],
-            'budget': [442, 525, 511, 593, 546, 564, 617]
+            'budget': 170,
+            'costs': [41, 50, 49, 59, 55, 57, 60],
+            'ratings': [442, 525, 511, 593, 546, 564, 617]
         },
         'output': 1735
     }
 
     test4 = {
         'input': {
-            'costs': 15,
-            'ratings': [4, 5, 6],
-            'budget': [1, 2, 3]
+            'budget': 15,
+            'costs': [4, 5, 6],
+            'ratings': [1, 2, 3]
         },
         'output': 6
     }
 
     test5 = {
         'input': {
-            'costs': 15,
-            'ratings': [4, 5, 1, 3, 2, 5],
-            'budget': [2, 3, 1, 5, 4, 7]
+            'budget': 15,
+            'costs': [4, 5, 1, 3, 2, 5],
+            'ratings': [2, 3, 1, 5, 4, 7]
         },
         'output': 19
     }
